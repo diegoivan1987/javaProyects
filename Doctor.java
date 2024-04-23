48569588b4e8b0ceca98564bf59a12827fe1b265
Diego Ivan <diegoivan1987@outlook.com>
2024-04-22 20:17:07 -0600

    sobrecarga de metodos

--- Doctor.java
+++ Doctor.java
@@ -1,22 +1,29 @@
-import javax.print.Doc;
 
 public class Doctor {
     //Atributos
-    int id;
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
 
     Doctor(){
         System.out.println("Construyendo el Objeto Doctor");
+
     }
 
-    Doctor(String name){
+    Doctor(String name, String speciality){
         System.out.println("El nombre del Doctor asignado es: " + name);
+        id++;
+        this.name = name;
+        this.speciality = speciality;
     }
 
     //Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: " + id);
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
+import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor("AnahÃ­ Salgado");
-        myDoctor.name = "Alejandro RodrÃ­guez";
-        myDoctor.showName();
+        Doctor myDoctor = new Doctor("AnahÃ­ Salgado","Pediatria");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
 
-        UIMenu.showMenu();
+        showMenu();
     }
 }
