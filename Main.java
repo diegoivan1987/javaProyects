9cacc8a353916d5073da23b09f3d053df9b087b4
Diego Ivan <diegoivan1987@outlook.com>
2024-04-22 19:53:40 -0600

    metodos estaticos

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Doctor myDoctor = new Doctor("AnahÃ­ Salgado");
+        myDoctor.name = "Alejandro RodrÃ­guez";
+        myDoctor.showName();
+
+        UIMenu.showMenu();
+    }
+}
